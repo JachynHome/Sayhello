fb17d5b0ebfce1defbeca43cbc6f8f76609b190c
JachynHome <63940267+JachynHome@users.noreply.github.com>
2021-02-21 08:15:46 +0800

    Add files via upload

---
+++ sayhello/sayhello/sayhello.cpp
@@ -0,0 +1,5 @@
+﻿#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, PSTR szCmdLine, int iCmdShow) {
+	MessageBox(NULL, TEXT("我的第一个win程序。"), TEXT("SayHello"), MB_OK);
+	return 0;
+}
